e0d1ca833a7656e356b7d43270bc5ac7e2520b19
tuorqai <tuorqai@gmail.com>
2023-06-15 00:41:53 +0900

    Document some of the functions.

--- include/libqu/libqu.h
+++ include/libqu/libqu.h
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 
 //------------------------------------------------------------------------------
+// Exported function attribute
 
 #if defined(QU_SHARED)
 #   if defined(_MSC_VER)
@@ -29,11 +30,17 @@
 #   define QU_API
 #endif
 
+//------------------------------------------------------------------------------
+// Calling conventions
+
 #if defined(_WIN32)
 #   define QU_CALL __cdecl
 #else
 #   define QU_CALL
 #endif
+
+//------------------------------------------------------------------------------
+// Non-return function attribute
 
 #if defined(_MSC_VER)
 #   define QU_NO_RET __declspec(noreturn)
@@ -44,21 +51,64 @@
 #endif
 
 //------------------------------------------------------------------------------
+// Constants and macros
 
+/**
+ * \brief Approximate value of Pi.
+ */
 #define QU_PI               3.14159265358979323846
-#define QU_DEG2RAD(Deg)     ((Deg) * (QU_PI / 180.0))
-#define QU_RAD2DEG(Rad)     ((Rad) * (180.0 / QU_PI))
-#define QU_MAX(A, B)        ((A) > (B) ? (A) : (B))
-#define QU_MIN(A, B)        ((A) < (B) ? (A) : (B))
 
-#define QU_COLOR(Red, Green, Blue) \
-    (255 << 24 | (Red) << 16 | (Green) << 8 | (Blue))
+/**
+ * \brief Convert degrees to radians.
+ * \param deg Angle in degrees.
+ */
+#define QU_DEG2RAD(deg)     ((deg) * (QU_PI / 180.0))
 
-#define QU_RGBA(Red, Green, Blue, Alpha) \
-    ((Alpha) << 24 | (Red) << 16 | (Green) << 8 | (Blue))
+/**
+ * \brief Convert radians to degrees.
+ * \param rad Angle in radians.
+ */
+#define QU_RAD2DEG(rad)     ((rad) * (180.0 / QU_PI))
+
+/**
+ * \brief Get maximum of two values.
+ * \param a First value.
+ * \param b Second value.
+ */
+#define QU_MAX(a, b)        ((a) > (b) ? (a) : (b))
+
+/**
+ * \brief Get minimum of two values.
+ * \param a First value.
+ * \param b Second value.
+ */
+#define QU_MIN(a, b)        ((a) < (b) ? (a) : (b))
+
+/**
+ * \brief Get color value from individual RGB components.
+ * \param red Red component (in range 0-255).
+ * \param green Green component (in range 0-255).
+ * \param blue Blue component (in range 0-255).
+ */
+#define QU_COLOR(red, green, blue) \
+    (255 << 24 | (red) << 16 | (green) << 8 | (blue))
+
+/**
+ * \brief Get color value from individual RGBA components.
+ * \param red Red component (in range 0-255).
+ * \param green Green component (in range 0-255).
+ * \param blue Blue component (in range 0-255).
+ * \param alpha Alpha component (in range 0-255).
+ */
+#define QU_RGBA(red, green, blue, alpha) \
+    ((alpha) << 24 | (red) << 16 | (green) << 8 | (blue))
 
 //------------------------------------------------------------------------------
+// Enums
 
+/**
+ * \brief Keys of keyboard.
+ */
 typedef enum qu_key
 {
     QU_KEY_0,
@@ -169,6 +219,9 @@ typedef enum qu_key
     QU_TOTAL_KEYS,
 } qu_key;
 
+/**
+ * \brief Mouse buttons.
+ */
 typedef enum qu_mouse_button
 {
     QU_MOUSE_BUTTON_LEFT,
@@ -178,6 +231,9 @@ typedef enum qu_mouse_button
     QU_TOTAL_MOUSE_BUTTONS,
 } qu_mouse_button;
 
+/**
+ * \brief Bitmasks of mouse buttons.
+ */
 typedef enum qu_mouse_button_bits
 {
     QU_MOUSE_BUTTON_LEFT_BIT = (1 << QU_MOUSE_BUTTON_LEFT),
@@ -185,6 +241,9 @@ typedef enum qu_mouse_button_bits
     QU_MOUSE_BUTTON_MIDDLE_BIT = (1 << QU_MOUSE_BUTTON_MIDDLE),
 } qu_mouse_button_bits;
 
+/**
+ * \brief Screen modes.
+ */
 typedef enum qu_screen_mode
 {
     QU_SCREEN_MODE_DEFAULT,
@@ -193,53 +252,84 @@ typedef enum qu_screen_mode
 } qu_screen_mode;
 
 //------------------------------------------------------------------------------
+// Typedefs and structs
 
+/**
+ * \brief Color type.
+ * 
+ * Assumed to hold components in ARGB order.
+ */
 typedef uint64_t qu_color;
 
-//------------------------------------------------------------------------------
-
+/**
+ * \brief Two-dimensional floating-point vector.
+ */
 typedef struct qu_vec2f
 {
     float x;
     float y;
 } qu_vec2f;
 
+/**
+ * \brief Two-dimensional integer vector.
+ */
 typedef struct qu_vec2i
 {
     int x;
     int y;
 } qu_vec2i;
 
+/**
+ * \brief Texture handle.
+ */
 typedef struct qu_texture
 {
     int32_t id;
 } qu_texture;
 
+/**
+ * \brief Surface handle.
+ */
 typedef struct qu_surface
 {
     int32_t id;
 } qu_surface;
 
+/**
+ * \brief Font handle.
+ */
 typedef struct qu_font
 {
     int32_t id;
 } qu_font;
 
+/**
+ * \brief Sound handle.
+ */
 typedef struct qu_sound
 {
     int32_t id;
 } qu_sound;
 
+/**
+ * \brief Music handle.
+ */
 typedef struct qu_music
 {
     int32_t id;
 } qu_music;
 
+/**
+ * \brief Audio stream handle.
+ */
 typedef struct qu_stream
 {
     int32_t id;
 } qu_stream;
 
+/**
+ * \brief Initialization parameters.
+ */
 typedef struct qu_params
 {
     char const *title;
@@ -249,136 +339,420 @@ typedef struct qu_params
 } qu_params;
 
 //------------------------------------------------------------------------------
+// Function typedefs
 
+/**
+ * \brief Callback function for the main loop.
+ * \return False if the loop should stop, and true otherwise.
+ */
 typedef bool (*qu_loop_fn)(void);
+
+/**
+ * \brief Keyboard event callback.
+ */
 typedef void (*qu_key_fn)(qu_key key);
+
+/**
+ * \brief Mouse button event callback.
+ */
 typedef void (*qu_mouse_button_fn)(qu_mouse_button button);
+
+/**
+ * \brief Mouse wheel event callback.
+ */
 typedef void (*qu_mouse_wheel_fn)(int x_delta, int y_delta);
+
+/**
+ * \brief Mouse cursor event callback.
+ */
 typedef void (*qu_mouse_cursor_fn)(int x_delta, int y_delta);
 
 //------------------------------------------------------------------------------
+// Function prototypes
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
 //------------------------------------------------------------------------------
+// Base
 
-// Initialize the library. One can use NULL for default parameters.
+/**
+ * \brief Initialize the library with the specified parameters.
+ *
+ * This function initializes the library with the provided parameters.
+ * If the `params` argument is set to NULL, default parameters will be used.
+ *
+ * \param params A pointer to a structure containing initialization parameters.
+ *               Use NULL to use default parameters.
+ */
 QU_API void QU_CALL qu_initialize(qu_params const *params);
 
-// Terminate the library.
+/**
+ * \brief Terminate the library and clean up resources.
+ *
+ * This function terminates the library and performs necessary clean-up
+ * operations to release allocated resources.
+ * After calling this function, the library is no longer usable until it is
+ * initialized again.
+ */
 QU_API void QU_CALL qu_terminate(void);
 
-// Process user input. Returns false if the user requested exit.
-// It's recommended to call this every frame.
+/**
+ * \brief Process user input.
+ * 
+ * This function processes user input and updates internal state accordingly.
+ * It should be called every frame to handle user input.
+ * 
+ * \return Returns false if the user wants to close the application/game, and
+ *         true otherwise.
+ */
 QU_API bool QU_CALL qu_process(void);
 
-// Enter main loop.
+/**
+ * \brief Starts the automatic game loop.
+ * 
+ * The automatic loop is the only way to handle game loop on Web and mobile
+ * platforms.
+ * This function does not return.
+ * 
+ * \param loop_fn Callback function that will be called every frame.
+ */
 QU_API QU_NO_RET void QU_CALL qu_execute(qu_loop_fn loop_fn);
 
-// Draw everything on screen.
+/**
+ * \brief Swap buffers.
+ * 
+ * This function should be called when the rendering is finished in order to
+ * put on the screen everything that was drawn so far.
+ */
 QU_API void QU_CALL qu_present(void);
 
 //------------------------------------------------------------------------------
+// Core
 
-// Get current keyboard state as an array of QU_TOTAL_KEYS bools.
+/**
+ * \brief Get current keyboard state.
+ * 
+ * \return Array of QU_TOTAL_KEYS boolean values.
+           An element corresponding to a key is true it's pressed.
+ */
 QU_API bool const * QU_CALL qu_get_keyboard_state(void);
 
-// Check if a key is pressed.
+/**
+ * \brief Check if a key is pressed.
+ * 
+ * \param key Key value.
+ * \return True if the specified key is pressed.
+ */
 QU_API bool QU_CALL qu_is_key_pressed(qu_key key);
 
-// Get current mouse button state as a bitmask.
+/**
+ * \brief Get current mouse button state.
+ * 
+ * \return Bitmask representing button state.
+ */
 QU_API uint8_t QU_CALL qu_get_mouse_button_state(void);
 
-// Check if a mouse button is pressed.
+/**
+ * \brief Check if mouse button is pressed.
+ * 
+ * \param button Mouse button.
+ * \return True if the button is pressed, otherwise false.
+ */
 QU_API bool QU_CALL qu_is_mouse_button_pressed(qu_mouse_button button);
 
-// Get current mouse cursor position relative to the window.
+/**
+ * \brief Get mouse cursor position.
+ * Cursor position is relative to the window.
+ * Top-left corner of the window is (0, 0) coordinate.
+ * Cursor position is not updated if it's outside of the window.
+ * 
+ * \return Cursor position in a 2D vector.
+ */
 QU_API qu_vec2i QU_CALL qu_get_mouse_cursor_position(void);
 
-// Get mouse cursor movement amout since the last frame.
+/**
+ * \brief Get mouse cursor movement delta.
+ * This function returns how far cursor moved since the last frame.
+ * 
+ * \return Cursor movement delta in a 2D vector.
+ */
 QU_API qu_vec2i QU_CALL qu_get_mouse_cursor_delta(void);
 
-// Get mouse wheel movement amount since the last frame.
+/**
+ * \brief Get mouse wheel movement delta.
+ * This function returns how much wheel scrolled since the last frame.
+ *
+ * \return Wheel movement delta in a 2D vector.
+ */
 QU_API qu_vec2i QU_CALL qu_get_mouse_wheel_delta(void);
 
-// Get how many joysticks are connected.
-QU_API bool QU_CALL qu_is_joystick_connected(int);
+/**
+ * \brief Check if joystick is connected.
+ * This function can slow down a program, it's not recommended
+ * to use it too frequently (e. g. every frame).
+ * Joystick numbers start with 0, the joystick #0 is the first one,
+ * #1 is the second etc.
+ *
+ * \param joystick Number of a joystick.
+ * \return True if the joystick is connected.
+ */
+QU_API bool QU_CALL qu_is_joystick_connected(int joystick);
 
-// Get joystick identifier.
+/**
+ * \brief Get identifier of joystick.
+ * Identifier is usually a product name.
+ * 
+ * \param joystick Number of a joystick.
+ * \return String containing joystick identifier.
+ */
 QU_API char const * QU_CALL qu_get_joystick_id(int joystick);
 
-// Get how many buttons joystick does have.
+/**
+ * \brief Get number of buttons joystick has.
+ * 
+ * \param joystick Number of a joystick.
+ * \return Number of buttons.
+ */
 QU_API int QU_CALL qu_get_joystick_button_count(int joystick);
 
-// Get how many axes joystick does have.
+/**
+ * \brief Get number of axes joystick has.
+ *
+ * \param joystick Number of a joystick.
+ * \return Number of axes.
+ */
 QU_API int QU_CALL qu_get_joystick_axis_count(int joystick);
 
-// Get the identifier of a joystick button.
+/**
+ * \brief Get the identifier of a joystick button.
+ *
+ * \param joystick Number of a joystick.
+ * \param button Number of a button.
+ * \return String containing button identifier.
+ */
 QU_API char const * QU_CALL qu_get_joystick_button_id(int joystick, int button);
 
-// Get the identifier of a joystick axis.
+/**
+ * \brief Get the identifier of a joystick axis.
+ *
+ * \param joystick Number of a joystick.
+ * \param axis Number of an axis.
+ * \return String containing axis identifier.
+ */
 QU_API char const * QU_CALL qu_get_joystick_axis_id(int joystick, int axis);
 
-// Check if a button is pressed.
+/**
+ * \brief Check if a joystick button is pressed.
+ *
+ * \param joystick Number of a joystick.
+ * \param button Number of a button.
+ * \return True if the button is pressed, otherwise false.
+ */
 QU_API bool QU_CALL qu_is_joystick_button_pressed(int joystick, int button);
 
-// Get joystick axis value.
+/**
+ * \brief Get the value of a joystick axis.
+ *
+ * \param joystick Number of a joystick.
+ * \param axis Number of an axis.
+ * \return Value of the axis from -1.0 to 1.0.
+ */
 QU_API float QU_CALL qu_get_joystick_axis_value(int joystick, int axis);
 
-// Set callback function that will be called when a key is pressed.
+/**
+ * \brief Set key press callback.
+ * The callback will be called if a key is pressed.
+ * 
+ * \param fn Key event callback.
+ */
 QU_API void QU_CALL qu_on_key_pressed(qu_key_fn fn);
 
-// Set callback function that will be called when a key is hold down.
+/**
+ * \brief Set key repeat callback.
+ * The callback will be repeatedly called if a key is hold down.
+ *
+ * \param fn Key event callback.
+ */
 QU_API void QU_CALL qu_on_key_repeated(qu_key_fn fn);
 
-// Set callback function that will be called when a key is released.
+/**
+ * \brief Set key repeat callback.
+ * The callback will be called if a key is released.
+ *
+ * \param fn Key event callback.
+ */
 QU_API void QU_CALL qu_on_key_released(qu_key_fn fn);
 
-// Set callback function that will be called when a mouse button is pressed.
+/**
+ * \brief Set mouse button press callback.
+ * The callback will be called if a mouse button is pressed.
+ *
+ * \param fn Mouse button event callback.
+ */
 QU_API void QU_CALL qu_on_mouse_button_pressed(qu_mouse_button_fn fn);
 
-// Set callback function that will be called when a mouse button is released.
+/**
+ * \brief Set mouse button release callback.
+ * The callback will be called if a mouse button is released.
+ *
+ * \param fn Mouse button event callback.
+ */
 QU_API void QU_CALL qu_on_mouse_button_released(qu_mouse_button_fn fn);
 
-// Set callback function that will be called when a mouse cursor is moved.
+/**
+ * \brief Set the mouse cursor movement callback.
+ * 
+ * This callback will be triggered once per frame if the cursor position has
+ * changed since the last frame.
+ *
+ * \param fn Mouse cursor event callback.
+ */
 QU_API void QU_CALL qu_on_mouse_cursor_moved(qu_mouse_cursor_fn fn);
 
-// Set callback function that will be called when a mouse wheel is scrolled.
+/**
+ * \brief Set mouse cursor movement callback.
+ * The callback will be called once a frame if the mouse wheel was scrolled
+ * during last frame.
+ *
+ * \param fn Mouse wheel event callback.
+ */
 QU_API void QU_CALL qu_on_mouse_wheel_scrolled(qu_mouse_wheel_fn fn);
 
-// Get amount of time passed since the initialization. Medium precision.
+/**
+ * \brief Get medium-precision time.
+ * 
+ * This function returns the elapsed time in seconds since the library was
+ * initialized.
+ * 
+ * \return The elapsed time in seconds since the initialization, with
+ *         millisecond precision.
+ */
 QU_API float QU_CALL qu_get_time_mediump(void);
 
-// Get amount of time passed since the initialization. High precision.
+/**
+ * \brief Get high-precision time.
+ * 
+ * This function returns the elapsed time in seconds since the library was
+ * initialized.
+ *
+ * \return The elapsed time in seconds since the initialization, with
+ *         nanosecond precision.
+ */
 QU_API double QU_CALL qu_get_time_highp(void);
 
 //------------------------------------------------------------------------------
+// Graphics
 
+/**
+ * \brief Set the view parameters for rendering.
+ *
+ * This function sets the view parameters for rendering, including the position,
+ * width, height, and rotation.
+ *
+ * \param x The x-coordinate of the view position.
+ * \param y The y-coordinate of the view position.
+ * \param w The width of the view.
+ * \param h The height of the view.
+ * \param rotation The rotation angle of the view in degrees.
+ */
 QU_API void QU_CALL qu_set_view(float x, float y, float w, float h, float rotation);
+
+/**
+ * \brief Reset the view parameters to their default values.
+ *
+ * This function resets the view parameters to their default values, restoring
+ * the original view settings.
+ */
 QU_API void QU_CALL qu_reset_view(void);
 
-// Clear the screen.
+/**
+ * \brief Clear the screen with a specified color.
+ *
+ * This function clears the entire screen with the specified color.
+ *
+ * \param color The color to fill the screen with.
+ */
 QU_API void QU_CALL qu_clear(qu_color color);
 
-// Draw point.
+/**
+ * \brief Draw a point on the screen.
+ *
+ * This function draws a point on the screen at the specified coordinates with
+ * the specified color.
+ *
+ * \param x The x-coordinate of the point.
+ * \param y The y-coordinate of the point.
+ * \param color The color of the point.
+ */
 QU_API void QU_CALL qu_draw_point(float x, float y, qu_color color);
 
-// Draw line.
+/**
+ * \brief Draw a line on the screen.
+ *
+ * This function draws a line on the screen from point A (ax, ay) to point B
+ * (bx, by) with the specified color.
+ *
+ * \param ax The x-coordinate of point A.
+ * \param ay The y-coordinate of point A.
+ * \param bx The x-coordinate of point B.
+ * \param by The y-coordinate of point B.
+ * \param color The color of the line.
+ */
 QU_API void QU_CALL qu_draw_line(float ax, float ay, float bx, float by,
                                  qu_color color);
 
-// Draw triangle.
+/**
+ * \brief Draw a triangle on the screen.
+ *
+ * This function draws a triangle on the screen using the specified coordinates
+ * for its three points.
+ * The triangle can have both an outline and a fill color.
+ *
+ * \param ax The x-coordinate of the first point.
+ * \param ay The y-coordinate of the first point.
+ * \param bx The x-coordinate of the second point.
+ * \param by The y-coordinate of the second point.
+ * \param cx The x-coordinate of the third point.
+ * \param cy The y-coordinate of the third point.
+ * \param outline The color of the triangle's outline.
+ * \param fill The color to fill the triangle with.
+ */
 QU_API void QU_CALL qu_draw_triangle(float ax, float ay, float bx, float by,
                                      float cx, float cy, qu_color outline,
                                      qu_color fill);
 
-// Draw rectangle.
+/**
+ * \brief Draw a rectangle on the screen.
+ *
+ * This function draws a rectangle on the screen using the specified
+ * coordinates, width, and height.
+ * The rectangle can have both an outline and a fill color.
+ *
+ * \param x The x-coordinate of the top-left corner of the rectangle.
+ * \param y The y-coordinate of the top-left corner of the rectangle.
+ * \param w The width of the rectangle.
+ * \param h The height of the rectangle.
+ * \param outline The color of the rectangle's outline.
+ * \param fill The color to fill the rectangle with.
+ */
 QU_API void QU_CALL qu_draw_rectangle(float x, float y, float w, float h,
                                       qu_color outline, qu_color fill);
 
-// Draw circle.
+/**
+ * \brief Draw a circle on the screen.
+ *
+ * This function draws a circle on the screen with the specified center
+ * coordinates and radius.
+ * The circle can have both an outline and a fill color.
+ *
+ * \param x The x-coordinate of the center of the circle.
+ * \param y The y-coordinate of the center of the circle.
+ * \param radius The radius of the circle.
+ * \param outline The color of the circle's outline.
+ * \param fill The color to fill the circle with.
+ */
 QU_API void QU_CALL qu_draw_circle(float x, float y, float radius,
                                    qu_color outline, qu_color fill);
 
@@ -400,6 +774,7 @@ QU_API void QU_CALL qu_reset_surface(void);
 QU_API void QU_CALL qu_draw_surface(qu_surface surface, float x, float y, float w, float h);
 
 //------------------------------------------------------------------------------
+// Audio
 
 /**
  * Set master volume.
