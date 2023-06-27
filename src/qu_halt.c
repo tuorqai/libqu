c2961ca161e5a53e7f08a51665a84ae6ed4262b1
tuorqai <tuorqai@gmail.com>
2023-06-27 23:25:17 +0900

    halt: add Win32 message box

--- src/qu_halt.c
+++ src/qu_halt.c
@@ -27,6 +27,23 @@ void libqu_halt(char const *fmt, ...)
     }
 
     fprintf(stderr, "HALT: %s\n", heap ? heap : buffer);
+
+#ifdef _WIN32
+    size_t len = strlen(heap ? heap : buffer);
+    LPTSTR msg = malloc((len + 1) * sizeof(TCHAR));
+
+    if (msg) {
+#ifdef UNICODE
+        mbstowcs(msg, heap ? heap : buffer, len);
+#else
+        strncpy(msg, heap ? heap : buffer, len);
+#endif
+        msg[len] = 0;
+        MessageBox(NULL, msg, TEXT("libqu error"), MB_OK | MB_ICONERROR);
+        free(msg);
+    }
+#endif
+
     free(heap);
 
     abort();
